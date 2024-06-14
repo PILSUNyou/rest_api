10bf70cb8d9fe9de21be8cd30e9227ffaf5524f3
PILSUNyou <dbyo1125@naver.com>
2024-06-14 15:41:11 +0900

    api versioning

--- src/main/java/com/example/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/example/jwt/domain/member/controller/MemberController.java
@@ -2,29 +2,52 @@ package com.example.jwt.domain.member.controller;
 
 import com.example.jwt.domain.member.entity.Member;
 import com.example.jwt.domain.member.service.MemberService;
+import com.example.jwt.global.rsData.RsData;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 public class MemberController {
     private final MemberService memberService;
 
     @Data
     public static class LoginRequest {
         @NotBlank
-        private String userName;
+        private String username;
 
         @NotBlank
         private String password;
     }
 
+    @Getter
+    @AllArgsConstructor
+    public static class LoginResponse {
+        private final String accessToken;
+    }
+
     @PostMapping("/login")
-    public Member login(@Valid @RequestBody LoginRequest loginRequest){
-        return memberService.findByUsername(loginRequest.getUserName()).orElse(null);
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp){
+
+        // 테스트용
+//        resp.addHeader("Authentication","JWT Token");
+
+        String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
+
+        resp.addHeader("Authentication", accessToken);
+        return RsData.of(
+                "S-1",
+                "엑세스 토큰 생성되었습니다.",
+                new LoginResponse(accessToken)
+        );
     }
 }
--- src/test/java/com/example/jwt/domain/member/MemberController.java
+++ src/test/java/com/example/jwt/domain/member/MemberController.java
@@ -7,13 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,7 +35,7 @@ class MemberControllerTest {
         // When
         ResultActions resultActions = mvc
                 .perform(
-                        post("/member/login")
+                        post("/api/v1/member/login")
                                 .content("""
                                         {
                                             "username": "user1",
@@ -46,5 +49,20 @@ class MemberControllerTest {
         // Then
         resultActions
                 .andExpect(status().is2xxSuccessful());
+
+        MvcResult mvcResult = resultActions.andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+
+        String authentication = response.getHeader("Authentication");
+
+        assertThat(authentication).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("로그인 토근 발급 검증")
+    void t2() throws Exception {
+
+
     }
 }
