4ab6e113cc48fa707d7a949660da5c660bb222f6
PILSUNyou <dbyo1125@naver.com>
2024-06-17 15:31:55 +0900

    add Article

---
+++ src/main/java/com/example/jwt/domain/article/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.jwt.domain.article.repository;
+
+import com.example.jwt.domain.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
