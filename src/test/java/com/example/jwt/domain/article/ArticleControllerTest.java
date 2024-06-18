c816dbb1803d347e4e95492358fc001109bed150
PILSUNyou <dbyo1125@naver.com>
2024-06-18 15:52:04 +0900

    article post

--- src/test/java/com/example/jwt/domain/article/ArticleControllerTest.java
+++ src/test/java/com/example/jwt/domain/article/ArticleControllerTest.java
@@ -2,17 +2,21 @@ package com.example.jwt.domain.article;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.*;
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,7 +32,6 @@ public class ArticleControllerTest {
     @Test
     @DisplayName("GET /articles")
     void t1() throws Exception {
-
         // When
         ResultActions resultActions = mvc
                 .perform(
@@ -41,5 +44,47 @@ public class ArticleControllerTest {
                 .andExpect(jsonPath("$.resultCode").value("S-1"))
                 .andExpect(jsonPath("$.msg").exists())
                 .andExpect(jsonPath("$.data.articles[0].id").exists());
+    }
+
+    @Test
+    @DisplayName("GET /articles/1")
+    void t2() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/articles/1")
+                )
+                .andDo(print());
+        // Then
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.resultCode").value("S-1"))
+                .andExpect(jsonPath("$.msg").exists())
+                .andExpect(jsonPath("$.data.articles.id").value(1));
+    }
+
+    @Test
+    @DisplayName("POST /articles/1")
+    @WithUserDetails("user1")
+    void t3() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/articles")
+                                .content("""
+                                        {
+                                            "subject" : "제목 new",
+                                            "content" : "내용 new"
+                                        }
+                                        """)
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+        // Then
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.resultCode").value("S-3"))
+                .andExpect(jsonPath("$.msg").exists())
+                .andExpect(jsonPath("$.data.articles").exists());
     }
 }
